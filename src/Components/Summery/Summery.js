628bf961b6eb9f496e1efcde81aa43129667cbbe
Sabbir Shuvo <Shabbir Ahmed Shuvo>
2022-05-25 01:08:15 +0600

    warning remove

--- src/Components/Summery/Summery.js
+++ src/Components/Summery/Summery.js
@@ -7,36 +7,36 @@ const Summery = () => {
                 <h2 className="text-center text-4xl mt-10 text-orange-500 font-bold">Our Business ratings... <br />
                     Trusted Persons are... </h2>
             </div>
-            <div class="stats shadow bg-indigo-500 w-full mt-12">
-                <div class="stat">
-                    <div class="stat-figure text-primary">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
+            <div className="stats shadow bg-indigo-500 w-full mt-12">
+                <div className="stat">
+                    <div className="stat-figure text-primary">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
                     </div>
-                    <div class="stat-title text-white">Total Deals</div>
-                    <div class="stat-value text-white">25.6K</div>
-                    <div class="stat-desc text-white">21% more than last month</div>
+                    <div className="stat-title text-white">Total Deals</div>
+                    <div className="stat-value text-white">25.6K</div>
+                    <div className="stat-desc text-white">21% more than last month</div>
                 </div>
 
-                <div class="stat">
-                    <div class="stat-figure text-secondary">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
+                <div className="stat">
+                    <div className="stat-figure text-secondary">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
                     </div>
-                    <div class="stat-title text-white">Dealership</div>
-                    <div class="stat-value text-secondary">2.6M</div>
-                    <div class="stat-desc text-white">21% more than last month</div>
+                    <div className="stat-title text-white">Dealership</div>
+                    <div className="stat-value text-secondary">2.6M</div>
+                    <div className="stat-desc text-white">21% more than last month</div>
                 </div>
 
-                <div class="stat">
-                    <div class="stat-figure text-secondary">
-                        <div class="avatar online">
-                            <div class="w-16 rounded-full">
+                <div className="stat">
+                    <div className="stat-figure text-secondary">
+                        <div className="avatar online">
+                            <div className="w-16 rounded-full">
                                 <img src="https://api.lorem.space/image/face?w=128&h=128" />
                             </div>
                         </div>
                     </div>
-                    <div class="stat-value">86%</div>
-                    <div class="stat-title text-white">Tasks done</div>
-                    <div class="stat-desc text-white">31 tasks remaining</div>
+                    <div className="stat-value">86%</div>
+                    <div className="stat-title text-white">Tasks done</div>
+                    <div className="stat-desc text-white">31 tasks remaining</div>
                 </div>
 
             </div>
