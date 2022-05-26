a9c2029c4f09f5b4d189e4bc0f836548c55447af
Sabbir Shuvo <Shabbir Ahmed Shuvo>
2022-05-26 15:22:02 +0600

    finally get from api

--- src/Components/Dashboard/MyOrder.js
+++ src/Components/Dashboard/MyOrder.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const MyOrder = () => {
+
+    const [orders, setOrders] = useState([]);
+    const [user, loading, error] = useAuthState(auth);
+
+    useEffect(() => {
+        if (user) {
+            fetch(`http://localhost:5000/order?toolBuyer=${user.email}`)
+                .then(res => res.json())
+                .then(data => setOrders(data));
+        }
+    }, [user])
+
     return (
         <div>
-            <h2 className='text-2xl'>My Orders</h2>
+            <h2 className='text-2xl'>My Orders : {orders.length}</h2>
+
+            <div class="overflow-x-auto">
+                <table class="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>date</th>
+                            <th>Item</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            orders.map(order => <tr>
+                                <th>1</th>
+                                <td>{order.buyerName}</td>
+                                <td>{order.date}</td>
+                                <td>{order.toolName}</td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
+
         </div>
     );
 };
