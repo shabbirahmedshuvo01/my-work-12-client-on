37417d51395f617c9fa6f10d4f1ac3eeb1d0c74a
Sabbir Shuvo <Shabbir Ahmed Shuvo>
2022-05-27 02:45:22 +0600

    my profile added

--- src/Components/MyProfile/MyProfileWork.js
+++ src/Components/MyProfile/MyProfileWork.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import shuvo from '../../images/about/dekstop.1.jpg'
 
 const MyProfileWork = () => {
     return (
         <div>
-            <div className='flex flex-wrap gap-40'>
+            <div className='text-center'>
+                <div className='inline-block align-middle '>
+                    <img style={{ height: '400px', width: '400px' }} src={shuvo} alt="" />
+                </div>
                 <div className='mx-20 mt-10'>
                     <h2>Shabbir Ahmed Shuvo</h2>
-                    <br />
                     <p>make a moment a lasting conveyable memory. Let us help build your tribe. <br />We love what we do and we do what our clients love and work with great <br /> clients all over the world to create thoughtful and purposeful websites</p>
                     <li>Student At : Mymensing Polytechnic Institute</li>
                     <li>Studyding at: 3rd Year </li>
@@ -15,9 +18,11 @@ const MyProfileWork = () => {
                     <li>Job experience: Lotto at salesman</li>
                     <li>Home: Mymensing</li>
                     <li>Ssc Batch: 2019</li>
-                </div>
-                <div>
-                    <img style={{ height: '400px', width: '400px' }} src={shuvo} alt="" />
+                    <Link to='https://final-project-2bb92.web.app/'>https://final-project-2bb92.web.app/</Link>
+                    <br />
+                    <Link to='https://my-dev-project63.web.app/'>https://my-dev-project63.web.app/</Link>
+                    <br />
+                    <Link to='https://programmingworkweb5-2388.netlify.app/'>https://programmingworkweb5-2388.netlify.app/</Link>
                 </div>
             </div>
         </div>
